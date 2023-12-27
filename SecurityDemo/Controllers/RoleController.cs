dea056d8e1b26f4f0fc19be1a8cbc9c87ccf6b4f
user <user@DESKTOP-SVU5GE4>
2023-12-27 11:50:27 +0530

    Add project files.

---
+++ SecurityDemo/Controllers/RoleController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace SecurityDemo.Controllers
+{
+    [Authorize(Roles = "Admin")]
+
+    public class RoleController : Controller
+    {
+        RoleManager<IdentityRole> roleManager;
+        public RoleController(RoleManager<IdentityRole> roleManager)
+        {
+            this.roleManager = roleManager;
+        }
+        public IActionResult Index()       //display role list
+        {
+            var roles = roleManager.Roles.ToList();
+            return View(roles);
+        }
+        public IActionResult Create()     //create list
+        {
+            return View(new IdentityRole());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole role)
+        {
+            await roleManager.CreateAsync(role);
+            return RedirectToAction("Index");
+        }
+    }
+}
+
